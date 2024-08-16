c04cb04438f752608725952e02c72fd190245e3e
Felipe Villani <felipevillani.2000@hotmail.com>
2024-08-15 21:06:25 -0300

    feat: add font inter

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter } from "@next/font/google"
 
 export const metadata: Metadata = {
   title: "Felipe Villani",
 };
+
+const inter = Inter({
+  weight: ['100','200','300','400','500','600','700'],
+  subsets: ['latin'],
+});
 
 export default function RootLayout({
   children,
@@ -11,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>{children}</body>
     </html>
   );
