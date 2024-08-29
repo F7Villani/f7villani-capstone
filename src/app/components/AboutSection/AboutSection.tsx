71b5af77f4b5be72ffecfabc2447e5ec507311d4
Felipe Villani <felipevillani.2000@hotmail.com>
2024-08-28 21:49:28 -0300

    feat: add translation at all sections

--- src/app/components/AboutSection/AboutSection.tsx
+++ src/app/components/AboutSection/AboutSection.tsx
@@ -1,37 +1,41 @@
+import { useTranslations } from "next-intl";
 import "./AboutSection.css";
 
 export default function About() {
+
+  const t = useTranslations("aboutMe"); 
+
   return (
     <section id="about">
-        <h3>Sobre mim</h3>
+        <h3>{t("title")}</h3>
         <hr />
         <div className="grid">
           <div className="who-am-i">
             <p>
               <img src="./assets/Foto_LinkedIn_fundo_preto.png" alt="picture of mine"></img>
-              Sou o Felipe Villani, formado em Engenharia da Computação no Instituto Mauá de Tecnologia. Entusiasmado por games, esportes e pelo mercado financeiro, estou sempre em busca de desafios que me permitam crescer profissionalmente. Movido pela busca contínua por conhecimento e pela aplicação prática das minhas habilidades em desenvolvimento de software, com foco em soluções limpas e eficientes. Além disso, valorizo o trabalho em equipe como meio para alcançar resultados excepcionais.
+              {t("description")}        
             </p>
           </div>
           <div className="technologies">
             <div className="technology">
               <img src="./assets/c-sharp.png" alt="" />
               <h5>C# (.NET)</h5>
-              <p>Desde o início da minha trajetória profissional, tenho atuado predominantemente com C#, envolvendo o desenvolvimento e manutenção de sistemas e APIs, geralmente utilizando o banco de dados MS SQL Server.</p>
+              <p>{t("csharpExperience")}</p>
             </div>
             <div className="technology">
               <img src="./assets/flutter-logo.png" alt="" />
               <h5>Flutter</h5>
-              <p>Fiz alguns aplicativos e um curso de Flutter. Com ele aprendi bastante sobre reatividade, gerenciadores de estado e também muito sobre Arquitetura Limpa.</p>
+              <p>{t("flutterExperience")}</p>
             </div>
             <div className="technology">
               <img src="./assets/python.png" alt="" />
               <h5>Python</h5>
-              <p>Durante meu período na faculdade foi a linguagem que mais utilizei, principalmente para dados e IA. Tenho um bom entendimento da linguaguem e de algumas bibliotecas, como Pandas, NumPy e SciKit-Learn.</p>
+              <p>{t("pythonExperience")}</p>
             </div>
             <div className="technology">
               <img src="./assets/JavaScript.png" alt="" />
               <h5>JavaScript/HTML/CSS</h5>
-              <p>Profissionalmente já tive que desenvolver e sustentar aplicações que utilizavam Angular, e afim de aprender mais sobre o esse mundo fiz um curso de desenvolvimento web e alguns projetos.</p>
+              <p>{t("javaScriptExperience")}</p>
             </div>
           </div>
         </div>
--- src/app/components/ContactSection/ContactSection.tsx
+++ src/app/components/ContactSection/ContactSection.tsx
@@ -1,13 +1,22 @@
+import { useTranslations } from "next-intl";
 import "./ContactSection.css";
 
 export default function Contact() {
+
+  const t = useTranslations("contact");
+
   return (
     <section id="contact">
-      <h3>Contato</h3>
+      <h3>{t("title")}</h3>
       <hr />
-      <p>Quer entrar em contato comigo?</p>
-      <p>Fique a vontade para me mandar um email!</p>
-      <a style={{ marginTop: "20px" }} href="mailto:felipevillani.2000@hotmail.com" className="gradient-button">Envie um Email</a>
+      <p>{t("firstMessage")}</p>
+      <p>{t("secondMessage")}</p>
+      <a
+        style={{ marginTop: "20px" }}
+        href="mailto:felipevillani.2000@hotmail.com"
+        className="gradient-button">
+        {t("buttonMessage")}
+      </a>
     </section>
   );
 }
